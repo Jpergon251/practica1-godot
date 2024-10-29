928592aebc17f06a9dd0df68e3b59c7fa2b86b48
Jpergon251 <114741595+Jpergon251@users.noreply.github.com>
2024-10-29 13:28:11 +0100

    Import Changes

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,6 +1,6 @@
 extends Node  # Asegúrate de que tu GameManager herede de Node
 
-const UI_SCENE = preload("res://scenes/UI.tscn")  # Carga la escena de UI
+@onready var ui: Control = $"../CanvasLayer/Ui"
 
 @onready var coins = $"../Coins"  # Ruta hacia el nodo Coins
 @onready var score_label = $ScoreLabel  # Ruta hacia el Label que muestra el puntaje
@@ -9,16 +9,16 @@ var total_coins
 var score = 0
 
 func _ready():
-	# Instanciar la escena de UI
-	var ui = UI_SCENE.instantiate()  
-	add_child(ui)  # Añadir la UI a la escena principal
-
+	
+	
 	total_coins = coins.get_child_count()  # Contar las monedas al inicio
 	update_score_label()  # Actualiza el label al inicio
 
 # Función para actualizar el texto del puntaje en el label
 func update_score_label():
 	score_label.text = "You collected " + str(score) + " coins from " + str(total_coins)
+	ui.actualizar_puntuacion(score, total_coins)
+	#print(ui.coins_text.text)
 	print(score_label.text)
 
 # Función para sumar un punto cuando se recoge una moneda
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,22 +1,9 @@
 extends Control
 
-
-@onready var label_puntuacion = $Panel/LabelPuntuacion
-@onready var progress_bar_vida = $Panel/ProgressBar
-
-var puntuacion = 0
-var vida = 100
+@onready var coins_text: Label = $Panel/CoinsText
 
 # Actualizar la puntuación en la UI
-func actualizar_puntuacion(nueva_puntuacion):
-	puntuacion = nueva_puntuacion
-	label_puntuacion.text = "Puntuación: %s" % puntuacion
-
-# Actualizar la barra de vida en la UI
-func actualizar_vida(nueva_vida):
-	vida = nueva_vida
-	progress_bar_vida.value = vida
-
-# Función para manejar un botón presionado (conectado desde el editor)
-func _on_BotonReiniciar_pressed():
-	get_tree().reload_current_scene()
+func actualizar_puntuacion(monedas_recogidas, monedas_totales):
+	
+	coins_text.text = "Monedicas: " + str(monedas_recogidas) + "/" + str(monedas_totales)
+	print(coins_text.text)	
